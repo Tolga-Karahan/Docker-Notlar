fa937321e04d930d5f3d059c0eb54422f78b06d6
tkarahan <karahantlg@gmail.com>
2020-04-20 14:37:42 +0300

    some additions

--- container-lifetime-and-persistent-data/persistent-data.sh
+++ container-lifetime-and-persistent-data/persistent-data.sh
@@ -7,15 +7,15 @@
 
 # Verileri containerdan bağımsız olarak saklamak için kullanabileceğimiz
 # yöntemlerden biri Dockerfile içerisinde VOLUME komutu ile diskte bir
-# yer belirtmektir. Böylece biz manul olarak silmedikçe veriler containerdan
+# yer belirtmektir. Böylece biz manuel olarak silmedikçe veriler containerdan
 # bağımsız olarak saklanır:
 VOLUME /var/lib/mysql
 
 # Container konfügürasyonunda, "Volumes" altında, saklanacak veriler için
 # oluşturulan volumeları görebiliriz. Volumelar için oluşturulan kayıtların
-# aynıları "Mounts" altında da görülebilir. Çünkü aslında hostun dosya 
-# sisteminde belirtilen pathe karşılık düşen ve container için unique bir
-# alan vardır ve veriler buraya yazılır.
+# aynıları "Mounts" altında da görülebilir. Çünkü aslında argüman olarak
+# verdiğimiz path container içinde kullanım içindir ve hostun diskinde
+# container için mount edilmiş unique bir disk alanına işaret eder.
 
 # Containerlar için oluşturulan volumeları şu şekilde görebiliriz:
 docker volume ls
@@ -27,3 +27,6 @@ docker volume ls
 # volumelara isim verebiliriz. Bunun için -v opsiyonunu ve volume-name:path
 # argümanını kullanıyoruz.
 docker container run -v mysql-db:/var/lib/mysql mysql
+
+# Eğer farklı bir driver kullanarak volume oluşturmak ve etiketlemek istiyorsak 
+# docker volume create komutunu kullanmalıyız. 
